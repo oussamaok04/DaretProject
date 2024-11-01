be5246699dd9ef5dcf4ab5a6c990d9c010ddc9c3
Oussama Okbani <oussama.okbani2013@gmail.com>
2024-11-01 09:38:11 +0100

    Creation de Gateway MS

---
+++ GetwayService/src/main/java/org/example/getwayservice/GetwayServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.getwayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetwayServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GetwayServiceApplication.class, args);
+    }
+
+}
