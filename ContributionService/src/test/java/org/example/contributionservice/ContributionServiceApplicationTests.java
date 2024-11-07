eab7c327c89ca459fb3336c0928256b74e1f6348
Oussama Okbani <oussama.okbani2013@gmail.com>
2024-11-07 23:38:46 +0100

    Creating contribution service

---
+++ ContributionService/src/test/java/org/example/contributionservice/ContributionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.contributionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContributionServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
