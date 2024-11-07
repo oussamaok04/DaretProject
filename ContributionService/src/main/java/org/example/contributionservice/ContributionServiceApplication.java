c4edcc40ccf7817dd370c2af8dbada82e36c935a
Oussama Okbani <oussama.okbani2013@gmail.com>
2024-11-07 23:37:56 +0100

    Enabling Discovery Client

---
+++ ContributionService/src/main/java/org/example/contributionservice/ContributionServiceApplication.java
@@ -0,0 +1,14 @@
+package org.example.contributionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication @EnableDiscoveryClient
+public class ContributionServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContributionServiceApplication.class, args);
+    }
+
+}
