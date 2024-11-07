f8019aef5dfaee3c2067671219e15f1cca1d7076
Oussama Okbani <oussama.okbani2013@gmail.com>
2024-11-07 23:35:09 +0100

    Fixing group table creation error: changing table name from group to groupDaret

--- GroupService/src/main/java/org/example/groupservice/entities/Group.java
+++ GroupService/src/main/java/org/example/groupservice/entities/Group.java
@@ -1,16 +1,17 @@
 package org.example.groupservice.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
+@NoArgsConstructor  @AllArgsConstructor
+@Table(name = "groupDaret")
 public class Group {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long groupId;
     private String groupName;
     private String description;
 }
