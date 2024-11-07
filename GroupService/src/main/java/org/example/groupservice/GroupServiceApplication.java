c3adc1dcae07d12d4043d760ddc9d0d52cb6a76c
Oussama Okbani <oussama.okbani2013@gmail.com>
2024-11-07 23:32:31 +0100

    Enabling Client Discovery

--- GroupService/src/main/java/org/example/groupservice/GroupServiceApplication.java
+++ GroupService/src/main/java/org/example/groupservice/GroupServiceApplication.java
@@ -2,8 +2,9 @@ package org.example.groupservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
+@SpringBootApplication @EnableDiscoveryClient
 public class GroupServiceApplication {
 
     public static void main(String[] args) {
