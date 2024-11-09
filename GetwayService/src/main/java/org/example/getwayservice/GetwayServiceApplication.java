46f2cb337184dc815695926dafa6d793cde36236
Oussama Okbani <oussama.okbani2013@gmail.com>
2024-11-09 01:47:37 +0100

    configuring gateway (static configuration)

--- GetwayService/src/main/java/org/example/getwayservice/GetwayServiceApplication.java
+++ GetwayService/src/main/java/org/example/getwayservice/GetwayServiceApplication.java
@@ -2,12 +2,24 @@ package org.example.getwayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+//@EnableDiscoveryClient
 public class GetwayServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GetwayServiceApplication.class, args);
     }
 
+//    @Bean
+//    public RouteLocator myRoutes(RouteLocatorBuilder builder){
+//        return builder.routes()
+//                .route(r -> r.path("").uri(""))
+//                .route(r -> r.path("").uri(""))
+//                .build();
+//    }
 }
