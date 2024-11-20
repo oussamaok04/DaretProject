c15521bf99697ebd0a11414fb1a87f04cfc98b77
Oussama Okbani <oussama.okbani2013@gmail.com>
2024-11-20 23:57:28 +0100

    dynamic configuration gateway

--- GetwayService/src/main/java/org/example/getwayservice/GetwayServiceApplication.java
+++ GetwayService/src/main/java/org/example/getwayservice/GetwayServiceApplication.java
@@ -3,16 +3,24 @@ package org.example.getwayservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 public class GetwayServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GetwayServiceApplication.class, args);
+    }
+
+    @Bean
+    DiscoveryClientRouteDefinitionLocator dynamicRoutes(ReactiveDiscoveryClient rdc, DiscoveryLocatorProperties dlp){
+        return new DiscoveryClientRouteDefinitionLocator(rdc, dlp);
     }
 
 //    @Bean
