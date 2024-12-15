99c9e800aca2321c18b81dacd03f7f11d6a69022
Oussama Okbani <oussama.okbani2013@gmail.com>
2024-12-15 02:16:38 +0100

    changed the signature of create and update to use DTO

--- GroupService/src/main/java/org/example/groupservice/services/GroupService.java
+++ GroupService/src/main/java/org/example/groupservice/services/GroupService.java
@@ -1,12 +1,13 @@
 package org.example.groupservice.services;
 
 import org.example.groupservice.entities.Group;
+import org.example.groupservice.entities.dto.GroupDTO;
 
 import java.util.List;
 
 public interface GroupService {
-    Group createGroup(Group group);
-    Group updateGroup(Group group);
+    Group createGroup(GroupDTO dto);
+    Group updateGroup(GroupDTO dto, Long groupId);
     Group getGroup(Long id);
     List<Group> getAllGroups();
     void deleteGroup(Long id);
