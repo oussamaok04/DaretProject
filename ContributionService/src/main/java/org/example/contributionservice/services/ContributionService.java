f947d3a8104b27d43f92b1668aa006c2c49f022e
Oussama Okbani <oussama.okbani2013@gmail.com>
2024-12-15 02:40:34 +0100

    changed create and update signatures so they can work with the contribution DTO

--- ContributionService/src/main/java/org/example/contributionservice/services/ContributionService.java
+++ ContributionService/src/main/java/org/example/contributionservice/services/ContributionService.java
@@ -1,13 +1,14 @@
 package org.example.contributionservice.services;
 
 import org.example.contributionservice.entities.Contribution;
+import org.example.contributionservice.entities.dto.ContributionDTO;
 
 import java.util.List;
 
 public interface ContributionService {
     public List<Contribution> getAllContributions();
     public Contribution getContributionById(Long id);
-    public Contribution createContribution(Contribution contribution);
-    public Contribution updateContribution(Contribution contribution);
+    public Contribution createContribution(ContributionDTO dto);
+    public Contribution updateContribution(ContributionDTO dto, Long contribuionId);
     public void deleteContribution(Long id);
 }
