6fbe62bac3a1e112057b9da6cb9682ebaca04f12
Oussama Okbani <oussama.okbani2013@gmail.com>
2024-12-15 02:39:23 +0100

    Added upadate and delete endpoints

--- ContributionService/src/main/java/org/example/contributionservice/controllers/ContributionController.java
+++ ContributionService/src/main/java/org/example/contributionservice/controllers/ContributionController.java
@@ -1,34 +1,43 @@
 package org.example.contributionservice.controllers;
 
 import org.example.contributionservice.entities.Contribution;
+import org.example.contributionservice.entities.dto.ContributionDTO;
 import org.example.contributionservice.services.ContributionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/contributions")
 public class ContributionController {
     @Autowired
     private ContributionService contributionService;
 
-    @GetMapping("/")
-    public String hello(){
-        return "Hello to contribution service";
+    @GetMapping("/all")
+    public ResponseEntity<List<Contribution>> getAllContributions() {
+        return ResponseEntity.ok(contributionService.getAllContributions());
     }
 
-    @GetMapping("/contributions/all")
-    public List<Contribution> getAllContributions() {
-        return contributionService.getAllContributions();
+    @GetMapping("/search/{id}")
+    public ResponseEntity<Contribution> getContributionById(@PathVariable("id") Long id) {
+        return ResponseEntity.ok(contributionService.getContributionById(id));
     }
 
-    @GetMapping("/contributions/{id}")
-    public Contribution getContributionById(@PathVariable("id") Long id) {
-        return contributionService.getContributionById(id);
+    @PostMapping("/save")
+    public ResponseEntity<Contribution> saveContribution(@RequestBody ContributionDTO contribution) {
+        return ResponseEntity.ok(contributionService.createContribution(contribution));
     }
 
-    @PostMapping("/contributions/save")
-    public Contribution saveContribution(@RequestBody Contribution contribution) {
-        return contributionService.createContribution(contribution);
+    @PutMapping("/update/{id}")
+    public ResponseEntity<Contribution> updateContribution(@PathVariable("id") Long id, @RequestBody ContributionDTO contribution) {
+        return ResponseEntity.ok(contributionService.updateContribution(contribution, id));
+    }
+
+    @DeleteMapping("/remove/{id}")
+    public ResponseEntity<String> removeContribution(@PathVariable("id") Long id) {
+        contributionService.deleteContribution(id);
+        return ResponseEntity.ok("Removed contribution with id " + id);
     }
 }
