6d2d719ea22d8176ffb3ff1fa77633fe40e2ace6
Oussama Okbani <oussama.okbani2013@gmail.com>
2024-12-15 02:14:45 +0100

    added some methodes to use later

--- GroupService/src/main/java/org/example/groupservice/services/GroupMemberService.java
+++ GroupService/src/main/java/org/example/groupservice/services/GroupMemberService.java
@@ -1,13 +1,18 @@
 package org.example.groupservice.services;
 
+import org.example.groupservice.entities.Group;
 import org.example.groupservice.entities.GroupMember;
+import org.example.groupservice.entities.dto.MemberDTO;
 
 import java.util.List;
 
 public interface GroupMemberService {
-    GroupMember createGroupMember(GroupMember groupMember);
-    GroupMember updateGroupMember(GroupMember groupMember);
+    GroupMember createGroupMember(MemberDTO dto);
+    GroupMember updateGroupMember(MemberDTO dto, Long memberId);
     GroupMember getGroupMember(Long id);
     List<GroupMember> getAllGroupMembers();
     void deleteGroupMember(Long id);
+    List<GroupMember> getGroupMembersByGroup(Long groupId);
+    GroupMember getGroupMemberByMemberEmail(String email);
+    GroupMember getGroupMemberByMemberPhone(String phone);
 }
