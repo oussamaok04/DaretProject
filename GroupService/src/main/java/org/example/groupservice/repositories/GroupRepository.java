ff40d75789ff87856655a4d23f752b81a96b613a
Oussama Okbani <oussama.okbani2013@gmail.com>
2024-12-15 02:16:02 +0100

    added some methods to use later

--- GroupService/src/main/java/org/example/groupservice/repositories/GroupRepository.java
+++ GroupService/src/main/java/org/example/groupservice/repositories/GroupRepository.java
@@ -4,6 +4,10 @@ import org.example.groupservice.entities.Group;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface GroupRepository extends JpaRepository<Group, Long> {
+    List<Group> findByGroupName(String name);
+    List<Group> findByGroupNameContaining(String name);
 }
