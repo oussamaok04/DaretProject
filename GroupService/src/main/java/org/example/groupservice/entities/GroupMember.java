1f0d68de7d285e469101c103c2ab43370145ffe9
Oussama Okbani <oussama.okbani2013@gmail.com>
2024-12-15 02:13:17 +0100

    adding more lombok annotations and more attributes and the JsonBackReference annotation to prevent dependency cycle when getting a member or a group

--- GroupService/src/main/java/org/example/groupservice/entities/GroupMember.java
+++ GroupService/src/main/java/org/example/groupservice/entities/GroupMember.java
@@ -1,13 +1,23 @@
 package org.example.groupservice.entities;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.example.groupservice.entities.dto.MemberDTO;
 
 @Entity
-@Data
+@Data @NoArgsConstructor @AllArgsConstructor @Builder
 public class GroupMember {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long groupMemberId;
+    private String memberName;
+    private String memberPhone;
+    private String memberEmail;
+
     @ManyToOne
+    @JsonBackReference
     private Group group;
 }
